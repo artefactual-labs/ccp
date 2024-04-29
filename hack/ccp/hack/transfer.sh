d478bcbbd0f23d63706a35743cfbe00bd62edadf
Jesús García Crespo <jesus@sevein.com>
2024-04-29 20:40:50 +0000

    Fix use of processingMCP.xml

--- hack/ccp/hack/transfer.sh
+++ hack/ccp/hack/transfer.sh
@@ -2,7 +2,10 @@
 
 set -x
 
+__dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 transfer=$(mktemp -d)
+cp $__dir/processingMCP.xml $transfer
 touch $transfer/hello.txt
 touch $transfer/bye.txt
 mv $transfer ~/.ccp/am-pipeline-data/watchedDirectories/activeTransfers/standardTransfer/
