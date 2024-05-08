07240336bc45246d225027933c275042180e120e
Jesús García Crespo <jesus@sevein.com>
2024-05-08 05:04:16 +0000

    Update transfer.sh to accept number of files

--- hack/ccp/hack/transfer.sh
+++ hack/ccp/hack/transfer.sh
@@ -5,7 +5,18 @@ set -x
 __dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 transfer=$(mktemp -d)
+
 cp $__dir/processingMCP.xml $transfer
-touch $transfer/hello.txt
-touch $transfer/bye.txt
+
+num_files=$1
+if [[ ! "$num_files" =~ ^[0-9]+$ ]]; then
+    touch "$transfer/hello.txt"
+    touch "$transfer/bye.txt"
+else
+    for (( i=0; i<num_files; i++ )); do
+        mktemp --tmpdir="$transfer" XXXXXXXX.txt
+    done
+fi
+
+
 mv $transfer ~/.ccp/am-pipeline-data/watchedDirectories/activeTransfers/standardTransfer/
