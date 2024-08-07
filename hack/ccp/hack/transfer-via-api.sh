2d355ebbf2cd6270660d544d342cded7bee29f77
Jesús García Crespo <jesus@sevein.com>
2024-08-07 17:53:27 +0000

    Include script to create transfers via the API

---
+++ hack/ccp/hack/transfer-via-api.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+grpcurl \
+  -plaintext \
+  -H "Authorization: ApiKey test:test" \
+  -d '{
+    "path": ["/home/archivematica/archivematica-sampledata/SampleTransfers/Images/pictures"],
+    "name": "Test",
+    "type": "TRANSFER_TYPE_STANDARD",
+    "autoApprove": true,
+    "processingConfig": "automated"
+  }' \
+    127.0.0.1:63030 \
+    archivematica.ccp.admin.v1beta1.AdminService.CreatePackage
