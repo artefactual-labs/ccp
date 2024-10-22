c7abec7241ba3e1d9c306e4cec2f1271e6ce4aa9
Jesús García Crespo <jesus@sevein.com>
2024-10-22 05:01:05 +0000

    Add worker command to Dagger

--- .dagger/go.mod
+++ .dagger/go.mod
@@ -26,16 +26,16 @@ require (
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.22.0 // indirect
 	github.com/sergi/go-diff v1.3.2-0.20230802210424-5b0b94c5c0d3 // indirect
 	github.com/sosodev/duration v1.3.1 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.28.0 // indirect
 	go.opentelemetry.io/otel/metric v1.28.0 // indirect
-	golang.org/x/net v0.26.0 // indirect
-	golang.org/x/sys v0.21.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20240701130421-f6361c86f094 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240701130421-f6361c86f094 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240814211410-ddb44dafa142 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 )
 
---
+++ .dagger/worker.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"unsafe"
+
+	"dagger/ccp/internal/dagger"
+)
+
+func (m *CCP) Worker() *Worker {
+	return &Worker{}
+}
+
+type Worker struct{}
+
+// AnalyzeClientModules generates a report of all Archivematica client modules.
+//
+// $ dagger call worker analyze-client-modules --dir=https://github.com/artefactual/archivematica#v1.17.0-rc.2:/src/MCPClient/lib/clientScripts
+func (m *Worker) AnalyzeClientModules(
+	ctx context.Context,
+	// +defaultPath="/worker/worker/clientScripts"
+	// +ignore=["__init__.py", "__pycache__", "*.pyc", "lib"]
+	dir *dagger.Directory,
+) *ClientModulesReport {
+	return &ClientModulesReport{
+		Dir: dir.WithoutFiles([]string{"__init__.py", "__pycache__", "*.pyc", "lib"}),
+	}
+}
+
+type ClientModulesReport struct {
+	Dir *dagger.Directory
+}
+
+func (r *ClientModulesReport) Review(ctx context.Context) (*dagger.Container, error) {
+	report, err := r.generate(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	ctr := dag.
+		Container().
+		From("alpine:edge").
+		WithExec([]string{"apk", "add", "--no-cache", "uv", "python3"}).
+		WithExec([]string{"uv", "tool", "install", "jtbl"}).
+		WithFile("report.json", report).
+		Terminal(dagger.ContainerTerminalOpts{
+			Cmd: []string{"sh", "-c", `cat report.json | /root/.local/bin/jtbl -n | less -S`},
+		})
+
+	return ctr, nil
+}
+
+func (r *ClientModulesReport) generate(ctx context.Context) (*dagger.File, error) {
+	mods := []*ClientModule{}
+
+	entries, err := r.Dir.Entries(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, entry := range entries {
+		report, err := analyzeModule(ctx, r.Dir.File(entry))
+		if err != nil {
+			return nil, err
+		}
+		mods = append(mods, report)
+	}
+
+	blob, err := json.Marshal(mods)
+	if err != nil {
+		return nil, err
+	}
+
+	f := dag.Directory().WithNewFile("report.json", string(blob)).File("report.json")
+
+	return f, nil
+}
+
+type ClientModule struct {
+	Name     string
+	Size     string
+	Callable bool
+	contents []byte
+}
+
+func analyzeModule(ctx context.Context, file *dagger.File) (*ClientModule, error) {
+	report, err := newModReport(ctx, file)
+	if err != nil {
+		return nil, err
+	}
+
+	if bytes.Contains(report.contents, []byte("def call(jobs")) {
+		report.Callable = true
+	}
+
+	return report, nil
+}
+
+func newModReport(ctx context.Context, file *dagger.File) (*ClientModule, error) {
+	report := &ClientModule{}
+
+	if name, err := file.Name(ctx); err != nil {
+		return nil, err
+	} else {
+		report.Name = name
+	}
+
+	if size, err := file.Size(ctx); err != nil {
+		return nil, err
+	} else {
+		kbs := float64(size) / 1024.0
+		report.Size = fmt.Sprintf("%.2f KB", kbs)
+	}
+
+	contents, err := file.Contents(ctx)
+	if err != nil {
+		return report, err
+	}
+	report.contents = unsafe.Slice(unsafe.StringData(contents), len(contents))
+
+	return report, nil
+}
